66003e1223598140bcfaf4040766933b46de9258
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2022-03-30 00:17:27 -0600

    Buggy code

--- src/Scenes/PlayerScene/Player.gd
+++ src/Scenes/PlayerScene/Player.gd
@@ -5,8 +5,15 @@ onready var sprite := $Sprite
 
 export var speed := 200
 
+const TILE_SIZE := 16
+
 var velocity := Vector2.LEFT * speed
-var map: TileMap
+var map: Node2D
+var direction = Vector2.LEFT
+var target_cell: Vector2
+var current_cell: Vector2
+
+var tile_map = map.get_node("Walkable")
 
 enum MOVE {UP, DOWN, RIGHT, LEFT}
 
@@ -14,11 +21,12 @@ var input_direction = MOVE.LEFT
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _physics_process(delta):
 	# For spawning in diferent sides of the map
+
 	if position.x < -16:
 		position.x = 448
 		print(position)
@@ -28,6 +36,9 @@ func _physics_process(delta):
 
 	input_direction = take_input()
 	# Validate if the direction is valid
+	if direction == Vector2.UP or direction == Vector2.DOWN:
+		target_cell = (input_direction * (TILE_SIZE/2)) + position
+		print(tile_map.get_cellv(tile_map.world_to_map(target_cell)))
 
 
 	# If the next tile is a wall stop movement
@@ -36,37 +47,29 @@ func _physics_process(delta):
 
 	move(delta)
 
+	direction = input_direction
+
 
 func move(delta) -> void:
-	if input_direction == MOVE.UP:
-		velocity = Vector2.UP * speed
-
-	if input_direction == MOVE.DOWN:
-		velocity = Vector2.DOWN * speed
-
-	if input_direction == MOVE.LEFT:
-		velocity = Vector2.LEFT * speed
-
-	if input_direction == MOVE.RIGHT:
-		velocity = Vector2.RIGHT * speed
+	velocity = input_direction * speed
 
 	position += velocity * delta
 
 
-func take_input() -> int:
+func take_input() -> Vector2:
 	if Input.is_action_just_pressed("ui_down"):
-		return MOVE.DOWN
+		return Vector2.DOWN
 
 	if Input.is_action_just_pressed("ui_up"):
-		return MOVE.UP
+		return Vector2.UP
 
 	if Input.is_action_just_pressed("ui_left"):
-		return MOVE.LEFT
+		return Vector2.LEFT
 
 	if Input.is_action_just_pressed("ui_right"):
-		return MOVE.RIGHT
+		return Vector2.RIGHT
 
-	return input_direction
+	return direction
 
 
 func set_position(location: Vector2) -> void:
